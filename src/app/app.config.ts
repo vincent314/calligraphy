7d2317490f91b0ea868b3ed5b3a19b8b32ab42ac
Vincent Marmin <vincent.marmin@gmail.com>
2024-12-09 11:58:43 +0100

    feat: setup PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from "@angular/common";
 
@@ -10,6 +10,7 @@ import { provideHttpClient } from "@angular/common/http";
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +18,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     { provide: LOCALE_ID, useValue: 'fr-FR' },
-    provideFirebaseApp(() => initializeApp({"projectId":"calligraphy-872ec","appId":"1:747694148235:web:5604545b97cba7a868e28e","databaseURL":"https://calligraphy-872ec-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"calligraphy-872ec.appspot.com","apiKey":"AIzaSyD5NTwZ98e58sU1FrqYhR8zAg3d6FIjOeg","authDomain":"calligraphy-872ec.firebaseapp.com","messagingSenderId":"747694148235"})), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()),
+    provideFirebaseApp(() => initializeApp({"projectId":"calligraphy-872ec","appId":"1:747694148235:web:5604545b97cba7a868e28e","databaseURL":"https://calligraphy-872ec-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"calligraphy-872ec.appspot.com","apiKey":"AIzaSyD5NTwZ98e58sU1FrqYhR8zAg3d6FIjOeg","authDomain":"calligraphy-872ec.firebaseapp.com","messagingSenderId":"747694148235"})), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
